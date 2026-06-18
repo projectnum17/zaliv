40621e8f9d835895838eb5bf6253be14078d14a1
projectnum17 <egor.chornyy@gmail.com>
2026-06-18 11:08:44 +0300

    init repo

---
+++ src/assets/js/app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import { example } from './modules/example';
+
+document.addEventListener('DOMContentLoaded', () => {
+    example();
+});
---
+++ src/assets/js/modules/example.js
@@ -0,0 +1,6 @@
+import { test } from './test';
+
+export const example = () => {
+    console.log(1);
+    test();
+};
